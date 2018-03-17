a14d6c22745f305db92bd0b3676f8a1b5ecaa98a
Roberto Vecilla <roberto.vecilla@gmail.com>
2018-03-17 19:59:58 +0000

    on app start if valid token found, sing in user automatically

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AsyncStorage } from 'react-native';
 import App from './App/App';
+import { AUTH_USER } from './App/Actions/Auth';
+import sStorage from './App/Actions/Storage';
 
 // redux
 import { createStore, applyMiddleware } from 'redux';
@@ -9,6 +11,12 @@ import appReducer from './App/Reducers';
 import thunk from 'redux-thunk';
 
 const store = createStore(appReducer, applyMiddleware(thunk));
+
+sStorage.getItem('token').then((token) => {
+    if(token) {
+        store.dispatch({ type: AUTH_USER });
+    }
+});
 
 // App
 const Reptceipts = () => (
