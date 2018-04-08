ccc57962e1eec9c6812106b91bf2a1b21cfebb6b
Roberto Vecilla <robvecilla@Robertos-MacBook-Pro.local>
2018-04-08 12:04:12 +0100

    added modules and packages

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { AppRegistry, AsyncStorage } from 'react-native';
 import App from './App/App';
 import { AUTH_USER } from './App/Actions/Auth';
 import sStorage from './App/Actions/Storage';
-
 // redux
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
