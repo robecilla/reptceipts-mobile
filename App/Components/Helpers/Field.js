a905ed128d26238f3246940f154d1cc3e784f8e2
Roberto Vecilla <robvecilla@Robertos-MacBook-Pro.local>
2018-04-08 12:00:41 +0100

    adapted field component for forms

--- App/Components/Helpers/Field.js
+++ App/Components/Helpers/Field.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-// import { TextInput } from 'react-native';
-import { View, TextInput, Text } from '@shoutem/ui';
+import { View, TextInput, Caption } from '@shoutem/ui';
+import { TextField } from 'react-native-material-textfield';
+import styles from './styles';
 
-
-const TextField = ({ input, placeholder, defaultValue, secureTextEntry, style,  meta: { touched, error }, ...custom }) => (
-    <View styleName="md-gutter-bottom rounded-corners">
-        <TextInput
-            defaultValue={defaultValue}
-            placeholder={placeholder}
-            secureTextEntry={secureTextEntry}
-            {...input}
-        />
-        {touched && (error && <Text styleName="md-gutter-left" style={{ color : 'red'}}>{error}</Text>)}
-    </View>
-);
-
-export default TextField;
+export default class Input extends React.Component {
+    render() {
+    let { input, title, placeholder, value, keyboardType, characterRestriction, secureTextEntry, meta: { touched, error }, ...custom } = this.props;
+    return (
+        <View styleName="sm-gutter">
+            <TextField
+                label={placeholder}
+                secureTextEntry={secureTextEntry}
+                keyboardType={keyboardType}
+                error={error && touched ? error : ''}
+                errorColor='#ff3860'
+                tintColor={styles.headerColor}
+                characterRestriction={characterRestriction}
+                title={title}
+                //value={'kepasa'}
+                {...input}
+            />
+        </View>
+    );
+    }
+}
